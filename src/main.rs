237a9739a38db0bab7853f7e299c70818d6a8579
WarriorsSami <sbarbutdica@gmail.com>
2021-11-23 10:47:53 +0200

    feat(main): test canvas

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#[macro_use]
+extern crate stdweb;
+
+mod canvas;
+mod direction;
+
+use canvas::Canvas;
+
 fn main() {
-    println!("Hello, world!");
+    stdweb::initialize();
+
+    let canvas = Canvas::new("#canvas", 20, 20);
+
+    canvas.draw(5, 5, "blue");
+    canvas.draw(12, 6, "red");
+    canvas.draw(12, 12, "green");
+    canvas.draw(6, 12, "yellow");
+
+    stdweb::event_loop();
 }
